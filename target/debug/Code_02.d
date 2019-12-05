3200ceca58f813fb3d232b440599f8d6efac59a8
Your Name <Your Email>
2019-12-05 08:29:51 +0000

    added commit

---
+++ target/debug/Code_02.d
@@ -0,0 +1,1 @@
+/home/nida/Code_02/Code_02/target/debug/Code_02: /home/nida/Code_02/Code_02/src/main.rs
---
+++ target/debug/deps/Code_02-f117776d97e7f2c1.d
@@ -0,0 +1,5 @@
+/home/nida/Code_02/Code_02/target/debug/deps/Code_02-f117776d97e7f2c1: src/main.rs
+
+/home/nida/Code_02/Code_02/target/debug/deps/Code_02-f117776d97e7f2c1.d: src/main.rs
+
+src/main.rs:
